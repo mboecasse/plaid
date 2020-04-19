71f805f8f51c573b43faf7b2d028556d39285c3e
jiraiya972 <jiraiya972@hotmail.com>
2020-04-19 22:17:19 +0200

    add session to update zoho

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ var util = require('util');
 var envvar = require('envvar');
 var express = require('express');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 var moment = require('moment');
 var plaid = require('plaid');
 var convertXml = require('xml-js');
-const superagent = require('superagent');
 var rp = require('request-promise');
 
 var APP_PORT = envvar.number('PORT', 8000);
+var APP_SECRET_SESSION = envvar.string('APP_SECRET_SESSION', 'dsfljdsjklfnsqfneilzuflez');
 var ZOHO_ACCESS_TOKEN = envvar.string('ZOHO_ACCESS_TOKEN');
 var PLAID_CLIENT_ID = envvar.string('PLAID_CLIENT_ID');
 var PLAID_SECRET = envvar.string('PLAID_SECRET');
@@ -70,6 +72,28 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json({ type: req => true }));
 
+
+var sess = {
+  secret: APP_SECRET_SESSION,
+  cookie: {
+    httpOnly: true,
+    maxAge: 6000000,
+    sameSite: true,
+  },
+  store: new FileStore({retries: 1}),
+  resave: false,
+  saveUninitialized: false,
+  unset: 'destroy',
+}
+ 
+if (app.get('env') === 'production') {
+  app.set('trust proxy', 1) // trust first proxy
+  sess.cookie.secure = true // serve secure cookies
+}
+ 
+app.use(session(sess))
+
+
 app.get('/', async function(request, response, next) {
   response.render('index.ejs', {
   //response.render('index.ejs', {
@@ -87,7 +111,7 @@ app.get('/', async function(request, response, next) {
 });
 
 app.get('/init-payment.html', async function(request, response, next) {
-  response.render('init_payment.ejs', {
+  response.render('init-payment.ejs', {
     PLAID_PUBLIC_KEY: PLAID_PUBLIC_KEY,
     PLAID_ENV: PLAID_ENV,
     PLAID_COUNTRY_CODES: PLAID_COUNTRY_CODES,
@@ -100,13 +124,13 @@ app.get('/init-payment.html', async function(request, response, next) {
 // This is an endpoint defined for the OAuth flow to redirect to.
 app.get('/confirm-payment.html', async function(request, response, next) {
   try {
-    const {ref_id: referenceId} = request.query;
+    const { referenceId } = request.session;
     const transactionStatus = 'confirmed';
 
-    const url = `https://creator.zoho.com/api/mboecasse1/xml/pardna-v3/form/Payment_Initiation/record/update?status=${transactionStatus}&criteria=reference_id%3D%22${referenceId}%22&authtoken=${ZOHO_ACCESS_TOKEN}`
-    const res = await rp(url);
+    const uri = `https://creator.zoho.com/api/mboecasse1/xml/pardna-v3/form/Payment_Initiation/record/update?status=${transactionStatus}&criteria=reference_id%3D%22${referenceId}%22&authtoken=${ZOHO_ACCESS_TOKEN}`;
+    console('POST : ', uri);
+    const res = await rp({method: 'POST', uri});
     const jsonResponse = extractPaymentInfoFromzoho(res);
-
     response.render('confirm-payment.ejs', {
       PLAID_PUBLIC_KEY: PLAID_PUBLIC_KEY,
       PLAID_ENV: PLAID_ENV,
@@ -118,13 +142,42 @@ app.get('/confirm-payment.html', async function(request, response, next) {
     response.render('error.ejs', {
       error
     });
+  } finally {
+    console.log('clean session')
+    delete request.session;
   }
 });
 
 
 
 // This is an endpoint defined for the OAuth flow to redirect to.
-app.get('/oauth-response.html', function(request, response, next) {
+app.get('/oauth-response.html', async function(request, response, next) {
+  try {
+    const { referenceId } = request.session;
+    const transactionStatus = 'confirmed';
+
+    const uri = `https://creator.zoho.com/api/mboecasse1/xml/pardna-v3/form/Payment_Initiation/record/update?status=${transactionStatus}&criteria=reference_id%3D%22${referenceId}%22&authtoken=${ZOHO_ACCESS_TOKEN}`;
+    console.log('POST : ', uri);
+    const res = await rp({method: 'POST', uri});
+    const jsonResponse = convertXml.xml2js(res, {compact: true});
+    prettyPrintResponse(jsonResponse);
+    response.render('confirm-payment.ejs', {
+      PLAID_PUBLIC_KEY: PLAID_PUBLIC_KEY,
+      PLAID_ENV: PLAID_ENV,
+      PLAID_PRODUCTS: PLAID_PRODUCTS,
+      PLAID_COUNTRY_CODES: PLAID_COUNTRY_CODES,
+      PLAID_OAUTH_NONCE: PLAID_OAUTH_NONCE,
+    });
+  } catch (error) {
+    console.error(error);
+    response.render('error.ejs', {
+      error
+    });
+  } finally {
+    console.log('clean session')
+    delete request.session;
+  }
+  /*
   response.render('oauth-response.ejs', {
     PLAID_PUBLIC_KEY: PLAID_PUBLIC_KEY,
     PLAID_ENV: PLAID_ENV,
@@ -132,6 +185,7 @@ app.get('/oauth-response.html', function(request, response, next) {
     PLAID_COUNTRY_CODES: PLAID_COUNTRY_CODES,
     PLAID_OAUTH_NONCE: PLAID_OAUTH_NONCE,
   });
+  */
 });
 
 // Exchange token flow - exchange a Link public_token for
@@ -502,29 +556,39 @@ const extractPaymentInfoFromzoho = (xml) => {
 // Sets the payment token in memory on the server side. We generate a new
 // payment token so that the developer is not required to supply one.
 // This makes the quickstart easier to use.
-app.post('/init_payment', async function(request, response, next) {
-  try {
-    const {transacId} =  request.body;
-    prettyPrintResponse(transacId);
-    const getTransactionsInformationUrl = `https://creator.zoho.com/api/xml/pardna-v3/view/All_Payment_Initiations?authtoken=${ZOHO_ACCESS_TOKEN}&criteria=reference_id%3D%3D%22${transacId}%22`;
-    // fetch from zoho all information for transcation
-    const res = await rp(getTransactionsInformationUrl);
-    const {currency, amount: value, reference_id: referenceId, recipient_id: recipientId} = extractPaymentInfoFromzoho(res);
+app.post('/init_payment', function(request, response, next) {
+  const handler = async (error) => {
+    try {
+      if (error) {
+        throw error;
+      }
+      const {transacId} =  request.body;
+      prettyPrintResponse(transacId);
+      const getTransactionsInformationUrl = `https://creator.zoho.com/api/xml/pardna-v3/view/All_Payment_Initiations?authtoken=${ZOHO_ACCESS_TOKEN}&criteria=reference_id%3D%3D%22${transacId}%22`;
+      // fetch from zoho all information for transcation
+      const res = await rp(getTransactionsInformationUrl);
+      const {currency, amount: value, reference_id: referenceId, recipient_id: recipientId} = extractPaymentInfoFromzoho(res);
 
-    // const recipientId = 'recipient-id-sandbox-01fc76e7-23ba-4d02-9a2e-2dadad2bcb85';
-    const { payment_id: paymentId } = await client.createPayment(
-      recipientId,
-      referenceId,
-      {currency, value: parseFloat(value)},
-    );
-    prettyPrintResponse(paymentId);
+      // const recipientId = 'recipient-id-sandbox-01fc76e7-23ba-4d02-9a2e-2dadad2bcb85';
+      const { payment_id: paymentId } = await client.createPayment(
+        recipientId,
+        referenceId,
+        {currency, value: parseFloat(value)},
+      );
+      prettyPrintResponse(paymentId);
 
-    const {  payment_token:paymentToken } = await client.createPaymentToken( paymentId );
-    PAYMENT_TOKEN = paymentToken;
-    PAYMENT_ID = paymentId;
-    return response.json({error: null, paymentToken});
-  } catch (error) {
-    prettyPrintResponse(error);
-    return response.json({ error: error });
+      const {  payment_token:paymentToken } = await client.createPaymentToken( paymentId );
+      PAYMENT_TOKEN = paymentToken;
+      PAYMENT_ID = paymentId;
+      
+      //save zoho_reference_id in session to finalize payment later
+      request.session.referenceId = referenceId;
+      return response.json({error: null, paymentToken});
+    } catch (error) {
+      prettyPrintResponse(error);
+      return response.json({ error: error });
+    }
   }
+
+  request.session.regenerate(handler);
 });
